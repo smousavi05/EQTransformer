412923319e8a12c970aa74def7f7e1a2bbe97b24
S.Mostafa Mousavi <smousavi05@users.noreply.github.com>
2022-07-30 11:49:51 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,21 +16,24 @@ setup(
     packages=find_packages(),
     keywords='Seismology, Earthquakes Detection, P&S Picking, Deep Learning, Attention Mechanism',
     install_requires=[
-	'pytest',
-	'numpy~=1.19.2',     # appox version: numpy 1.19.x but at least 1.19.2
-	'keyring>=15.1', 
-	'pkginfo>=1.4.2',
-	'scipy>=1.4.1', 
-	'tensorflow~=2.5.0', # tensorflow <2.7.0 needs numpy <1.20.0
-	'keras==2.3.1', 
-	'matplotlib', 
-	'pandas',
-	'tqdm>=4.48.0', 
-	'h5py~=3.1.0', 
-	'obspy',
-	'jupyter'], 
+	'pytest==7.1.2',
+	'numpy==1.22.4',     # appox version: numpy 1.19.x but at least 1.19.2
+	'keyring==23.7.0', 
+	'pkginfo==1.8.3',
+	'scipy==1.8.1',
+	'tensorflow-deps==2.9.0',
+	'tensorflow-estimator==2.9.0',
+	'tensorflow-macos==2.9.2',
+#	'tensorflow~=2.5.0', # tensorflow <2.7.0 needs numpy <1.20.0
+	'keras==2.9.0', 
+	'matplotlib-base==3.5.2', 
+	'pandas==1.4.3',
+	'tqdm==4.64.0', 
+	'h5py==3.6.0', 
+	'obspy==1.3.0',
+	'jupyter==1.0.0'], 
 
-    python_requires='>=3.6',
+    python_requires='==3.10.5',
 )
 
 
